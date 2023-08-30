6be1c736f9e66e8e23fcbe88940792858a46c4e8
unknown <le1823984@crc-lennox.qc.ca>
2023-08-30 12:07:25 -0400

    First commit/Cam flask server

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, Response, redirect, url_for
+from flask_httpauth import HTTPBasicAuth
+import cv2
+from cam2 import VideoCamera
+
+# this needs auth FLask httpauth
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame +
+               b'\r\n\r\n')
+
+@app.route('/vid_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/login')
+def login():
+    ...
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
