000d95a20c8bc9bee759fa75856688092a0eaf59
Alex <90349909+Hoookpoook@users.noreply.github.com>
2023-11-08 10:50:54 -0500

    cam class record

--- cam2.py
+++ cam2.py
@@ -1,8 +1,13 @@
 import cv2
+import time
+import threading
 
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.out = None
+        self.record_flag = False
         
     def __del__(self):
         self.video.release()
@@ -13,5 +18,22 @@ class VideoCamera(object):
         ret, jpeg = cv2.imencode('.jpg',frame)
         return jpeg.tobytes()
     
-    def rec(self,breaker):
-        ...
+    def wait_time(self):
+        time.sleep(10)
+        stop_recording()
+    
+    def start_recording():
+        self.out = cv2.VideoWriter('output.mp4', self.fourcc, 20.0, (640, 480))  
+        self.record_flag = True
+        t1 = threading.Thread(target=wait_time)
+        t1.start()
+        return 'all good'
+    
+    def stop_recording(): 
+        if self.out is not None:
+            self.out.release()
+            self.out = None
+        self.record_flag = False
+
+        
+    
